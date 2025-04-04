b2a41989b3fb63aa580484baf1242ed9d5b62573
Nikosane <linux.nikosane@gmail.com>
2025-04-04 23:11:51 +0530

    configuration file

---
+++ config.py
@@ -0,0 +1,6 @@
+# Simulation Configuration
+
+GRID_SIZE = (50, 50)         # Width x Height of the Petri Dish
+NUM_CELLS = 100              # Initial number of neural cells
+MAX_STEPS = 10000            # Total simulation steps
+SAVE_INTERVAL = 100          # Interval to save simulation state
